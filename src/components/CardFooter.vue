2791cac1188a6f2a4850e0e90c0a123ee6487aa2
Dudevss <patinyatiger1223@gmail.com>
2023-09-10 03:44:25 +0700

    Update feture hover link-icon

--- src/components/CardFooter.vue
+++ src/components/CardFooter.vue
@@ -2,19 +2,19 @@
   <div>
      <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
      <div class="text-center">
-        <a class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400">
+        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-blue-600 dark:hover:text-blue-400">
           <font-awesome-icon icon="fa-brands fa-linkedin"/>
         </a>
-        <a class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400">
+        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-blue-600 dark:hover:text-blue-400">
           <font-awesome-icon icon="fa-brands fa-facebook"/>
         </a>
-        <a class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400">
+        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-pink-600 dark:hover:text-pink-400">
           <font-awesome-icon icon="fa-brands fa-instagram"/>
         </a>
-        <a class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400">
+        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-purple-600 dark:hover:text-purple-400">
           <font-awesome-icon icon="fa-brands fa-github"/>
         </a>
-        <a class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400">
+        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-yellow-400 dark:hover:text-yellow-400">
           <font-awesome-icon icon="fa-solid fa-envelope"/>
         </a>
      </div>
