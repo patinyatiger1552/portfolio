799e2588e52870875f4dac2406f2083ccdc7b9a0
Dudevss <patinyatiger1223@gmail.com>
2023-09-11 00:15:00 +0700

    Update HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <img src="../assets/imgs/profile.jpg" class="w-[180px] h-[180px] sm:w-[200px] sm:h-[200px] mx-auto rounded-full shadow-md">
-    <h1 class="mt-8 mb-4 text-center text-3xl sm:text-4xl uppercase font-bold dark:text-white">Mr. Patinya pimpo</h1>
+    <h1 class="text-center mb-4 text-2xl sm:text-4xl uppercase font-bold dark:text-white">Portfolio</h1>
+    <img src="../assets/imgs/profile.jpg" class="w-[180px] h-[180px] sm:w-[200px] sm:h-[200px] mx-auto rounded-full drop-shadow-md hover:drop-shadow-2xl">
+    <h1 class="mt-8 mb-4 text-center text-3xl sm:text-4xl font-bold dark:text-white">Mr. Patinya pimpo</h1>
     <hr class="border-1 my-4 border-gray-400 w-14 mx-auto">
     <p class="text-center text-xl sm:text-2xl text-gray-700 dark:text-gray-300">A Fullstack Developer</p>
   </div>
