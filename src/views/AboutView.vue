893e276749a66c0ebb91c06e6f95d3cf038aefc4
Dudevss <patinyatiger1223@gmail.com>
2023-09-11 00:14:22 +0700

    Create content to AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,9 @@
 <template>
-  
+  <div class="text-center">
+    <img src="../assets/imgs/Duudevss.png" class="w-[180px] h-[180px] sm:w-[200px] sm:h-[200px] mx-auto rounded-full drop-shadow-md hover:drop-shadow-2xl mb-4">
+    <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! it's Patinya pimpo</h2>
+    <p class="text-justify sm:text-center my-4 sm:w-4/12 mx-auto dark:text-white">I am a junior programmer who likes to learn and develop new technologies. I like programming because I use my logical thinking skills. and problem solving To me, programming is like everyday life. If we encounter a problem, we just fix it. Because every problem always has a solution.</p>
+    <p class="text-center text-blue-500">#DotNetcore #WebApplication</p>
+  </div>
 </template>
 
