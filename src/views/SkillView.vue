94c950e5d60916ebcee4a31a07b0d6fa4feede73
Dudevss <patinyatiger1223@gmail.com>
2023-09-11 00:18:27 +0700

    Create content SkillView

---
+++ src/views/SkillView.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <!-- Skill Front-end -->
+    <h2 class="text-center mb-4 text-3xl font-bold dark:text-yellow-100">
+      Front-end
+    </h2>
+    <hr class="border-1 my-4 border-gray-400 w-14 mx-auto" />
+    <div class="text-center">
+      <a class="text-center m-1 p-1 text-gray-600 text-2xl hover:text-gray-600 dark:text-green-600">
+        <font-awesome-icon icon="fa-brands fa-vuejs" size="2xl" />
+      </a>
+       <a class="text-center m-1 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-blue-400">
+        <font-awesome-icon icon="fa-brands fa-react" size="2xl" />
+      </a>
+      <a class="text-center m-1 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-orange-600">
+        <font-awesome-icon icon="fa-brands fa-html5" size="2xl" />
+      </a>
+      <a class="text-center m-1 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-blue-500">
+        <font-awesome-icon icon="fa-brands fa-css3-alt" size="2xl" />
+      </a>
+      <a class="text-center m-1 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-yellow-500">
+        <font-awesome-icon icon="fa-brands fa-square-js" size="2xl" />
+      </a>
+      <a class="text-center m-1 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-purple-600">
+        <font-awesome-icon icon="fa-brands fa-bootstrap" size="2xl" />
+      </a>
+        <a class="text-center m-1 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-pink-400">
+        <font-awesome-icon icon="fa-brands fa-figma" size="2xl" />
+      </a>
+      <p class="text-center text-gray-600 p-4 dark:text-white">
+        Vue.js , React.js , HTML5 , CSS3 , JavascriptES6 , Bootstrap , Figma
+      </p>
+    </div>
+
+    <!-- Skill back-end -->
+    <hr class="border-1 my-4 border-gray-400 w-14 mx-auto" />
+    <h2 class="text-center mb-4 text-3xl font-bold dark:text-yellow-100">
+      Back-end
+    </h2>
+    <hr class="border-1 my-4 border-gray-400 w-14 mx-auto" />
+    <div class="text-center">
+      <a class="text-center m-2 p-2 text-gray-600 text-3xl hover:text-gray-600 dark:text-green-700">
+        <font-awesome-icon icon="fa-brands fa-node" size="2xl" />
+      </a>
+      <a class="text-center m-2 p-2 text-gray-600 text-3xl hover:text-gray-600 dark:text-blue-600">
+        <font-awesome-icon icon="fa-brands fa-php" size="2xl" />
+      </a>
+      <a class="text-center m-2 p-2 text-gray-600 text-3xl hover:text-gray-600 dark:text-yellow-400">
+        <font-awesome-icon icon="fa-brands fa-python" size="2xl" />
+      </a>
+      <p class="text-center text-gray-600 p-3 dark:text-white">
+        Nodejs , PHP , Python
+      </p>
+    </div>
+
+    <!-- tool -->
+    <hr class="border-1 my-4 border-gray-400 w-14 mx-auto" />
+    <h2 class="text-center mb-4 text-3xl font-bold dark:text-yellow-100">
+      Tools
+    </h2>
+    <hr class="border-1 my-4 border-gray-400 w-14 mx-auto" />
+    <div class="text-center">
+      <a class="text-center m-2 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-orange-600">
+        <font-awesome-icon icon="fa-brands fa-ubuntu" size="2xl" />
+      </a>
+      <a class="text-center m-2 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-blue-400">
+        <font-awesome-icon icon="fa-brands fa-docker" size="2xl" />
+      </a>
+       <a class="text-center m-2 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-purple-600">
+        <font-awesome-icon icon="fa-brands fa-slack" size="2xl" />
+      </a>
+       <a class="text-center m-2 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-orange-700">
+        <font-awesome-icon icon="fa-brands fa-square-git" size="2xl" />
+      </a>
+      <a class="text-center m-2 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-purple-400">
+        <font-awesome-icon icon="fa-brands fa-linux" size="2xl" />
+      </a>
+       <a class="text-center m-2 p-2 text-gray-600 text-2xl hover:text-gray-600 dark:text-red-600">
+        <font-awesome-icon icon="fa-brands fa-npm" size="2xl" />
+      </a>
+      <p class="text-center text-gray-600 p-3 dark:text-white">
+        Ubuntu2 , Docker , Slack , Git , Linux , Npm
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
