74c05b8eaa1322c69ec42904b748c3985af7b72f
Dudevss <patinyatiger1223@gmail.com>
2023-09-11 00:06:49 +0700

    Add link to profiles socials media

--- src/components/CardFooter.vue
+++ src/components/CardFooter.vue
@@ -1,26 +1,47 @@
 <template>
   <div>
-     <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
-     <div class="text-center">
-        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-blue-600 dark:hover:text-blue-400">
-          <font-awesome-icon icon="fa-brands fa-linkedin"/>
-        </a>
-        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-blue-600 dark:hover:text-blue-400">
-          <font-awesome-icon icon="fa-brands fa-facebook"/>
-        </a>
-        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-pink-600 dark:hover:text-pink-400">
-          <font-awesome-icon icon="fa-brands fa-instagram"/>
-        </a>
-        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-purple-600 dark:hover:text-purple-400">
-          <font-awesome-icon icon="fa-brands fa-github"/>
-        </a>
-        <a class="m-1 p-1 text-gray-500 text-2xl hover:text-yellow-400 dark:hover:text-yellow-400">
-          <font-awesome-icon icon="fa-solid fa-envelope"/>
-        </a>
-     </div>
+    <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto" />
+    <div class="text-center">
+      <a
+        href="https://www.linkedin.com/in/patinya-pimpo-87b9b0204/"
+        target="_blank"
+        class="m-1 p-1 text-gray-500 text-2xl hover:text-blue-600 dark:hover:text-blue-400">
+        <font-awesome-icon icon="fa-brands fa-linkedin" />
+      </a>
+      <a
+        href="https://www.facebook.com/patinya.pimpo"
+        target="_blank"
+        class="m-1 p-1 text-gray-500 text-2xl hover:text-blue-600 dark:hover:text-blue-400">
+        <font-awesome-icon icon="fa-brands fa-facebook" />
+      </a>
+      <a
+        href="https://www.instagram.com/tg_devstack799/"
+        target="_blank"
+        class="m-1 p-1 text-gray-500 text-2xl hover:text-pink-600 dark:hover:text-pink-400">
+        <font-awesome-icon icon="fa-brands fa-instagram" />
+      </a>
+      <a
+        href="https://github.com/patinyatiger1552"
+        target="_blank"
+        class="m-1 p-1 text-gray-500 text-2xl hover:text-purple-600 dark:hover:text-purple-400">
+        <font-awesome-icon icon="fa-brands fa-github" />
+      </a>
+      <a
+        href="https://dev.to/devger23"
+        target="_blank"
+        class="m-1 p-1 text-gray-500 text-2xl hover:text-blue-400 dark:hover:text-blue-200">
+        <font-awesome-icon icon="fa-brands fa-dev" />
+      </a>
+      <a
+        href="mailto:duudevss1223@gmail.com"
+        class="m-1 p-1 text-gray-500 text-2xl hover:text-yellow-400 dark:hover:text-yellow-400">
+        <font-awesome-icon icon="fa-solid fa-envelope" />
+      </a>
+    </div>
   </div>
+  <p class="text-center text-sm p-4 text-gray-700 dark:text-gray-300">
+    Copyright © 2023 Designed By Duudevss
+  </p>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
